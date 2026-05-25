9a1cd9e31a20577dbd1e5cb2971674a6fabc053e
Siphiwe Madela <siphiwemadela57@gmail.com>
2026-05-25 22:16:27 +0200

    Lesson 1

--- Lesson1_Siphiwe.py
+++ Lesson1_Siphiwe.py
@@ -3,5 +3,5 @@ name1 = input('Enter your name: ')
 age1 = int(input('Enter your age: '))
 height1 = float(input('Enter your height: '))
 
-print(f"Your name is", name1, " and you are", age1, " years old!. Your height is ", height1)
+print(f"Your name is", name1, "and you are", age1, " years old!. Your height is ", height1)
 
